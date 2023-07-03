37fb27ed2bbfe044b24d633300d29b6205f54c88
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-07-03 19:27:39 -0400

    updated files path

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,61 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
-const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
 
 const port = 3000;
 let publicUrl = `ws://localhost:${port}/ws`;
 //only for gitpod
-if(process.env.GITPOD_WORKSPACE_URL){
-  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
+if (process.env.GITPOD_WORKSPACE_URL) {
+  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split("://");
   publicUrl = `wss://${port}-${host}/ws`;
 }
 //only for codespaces
-if(process.env.CODESPACE_NAME){
+if (process.env.CODESPACE_NAME) {
   publicUrl = `wss://${process.env.CODESPACE_NAME}-${port}.preview.app.github.dev/ws`;
 }
 
 module.exports = {
-  entry: [
-    './src/js/index.js'
-  ],
+  entry: ["./src/App.jsx"],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+    publicPath: "/",
   },
   module: {
     rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          {
+            loader: "style-loader", // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+        ],
+      }, //css only files
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: { name: "[name].[ext]" },
         },
-        {
-          test: /\.(css)$/, use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }]
-        }, //css only files
-        { 
-          test: /\.(png|svg|jpg|gif)$/, use: {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' } 
-          }
-        }, //for images
-        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
-    ]
+      }, //for images
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        use: ["file-loader"],
+      }, //for fonts
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ["*", ".js", ".jsx"],
   },
   devtool: "source-map",
   devServer: {
@@ -61,7 +67,7 @@ module.exports = {
       directory: path.resolve(__dirname, "dist"),
     },
     client: {
-      webSocketURL: publicUrl
+      webSocketURL: publicUrl,
     },
   },
   plugins: [
@@ -70,8 +76,8 @@ module.exports = {
     //   files: path.resolve(__dirname, "src"),
     // }),
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
-        template: 'template.html'
+      favicon: "4geeks.ico",
+      template: "./public/index.html",
     }),
-  ]
+  ],
 };
