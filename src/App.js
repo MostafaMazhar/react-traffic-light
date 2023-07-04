24c9860745447f5ad757ea075f5be27fdf011a9c
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-07-04 13:55:03 -0400

    added interval function 3sec change color

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TrafficLight from "./component/TrafficLight.js";
 import "../src/styles/App.css";
 
 export default function App() {
-  const [currentColor, setCurrentColor] = React.useState("red"); //set default state to 'Red'
+  const [currentColor, setCurrentColor] = useState("red"); //set default state to 'Red'
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentColor((prevColor) => {
+        if (prevColor === "red") {
+          return "green";
+        } else if (prevColor === "green") {
+          return "yellow";
+        } else {
+          return "red";
+        }
+      });
+    }, 3000);
+  }, []); // <--- empty dependency array
 
   // const handleState = () => {
-  //   if (currentColor === "red") {
-  //     setCurrentColor("green");
-  //   } else if (currentColor === "green") {
-  //     setCurrentColor("yellow");
-  //   } else if (currentColor === "yellow") {
-  //     setCurrentColor("red");
-  //   }
+  //   setCurrentColor((prevState) => {
+  //     if (prevState === "red") return "green";
+  //     else if (prevState === "green") return "yellow";
+  //     else return "red";
+  //   });
   // };
 
-  const handleState = () => {
-    setCurrentColor((prevState) => {
-      if (prevState === "red") return "green";
-      else if (prevState === "green") return "yellow";
-      else return "red";
-    });
-  };
   return (
     <div className="app">
       <TrafficLight color={currentColor} />
-      <button className={`btn ${currentColor}`} onClick={handleState}>
-        Change Color
-      </button>
+      <button className={`btn ${currentColor}`}>Change Color</button>
+      {/* onClick={handleState} */}
     </div>
   );
 }
