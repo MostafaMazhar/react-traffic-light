c10540fa4a20dc24afc2354cc40ab14b578ac2b5
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-07-04 10:56:57 -0400

    traffficLight return jsx for color disply

--- src/component/TrafficLight.jsx
+++ src/component/TrafficLight.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function TrafficLight(props) {
-  return;
+export default function TrafficLight({ color }) {
+  return (
+    <div className="traffic-light">
+      <div className={`light red ${color === "red" ? "active" : ""}`}></div>
+      <div
+        className={`light yellow ${color === "yellow" ? "active" : ""}`}
+      ></div>
+      <div className={`light green ${color === "green" ? "active" : ""}`}></div>
+    </div>
+  );
 }
